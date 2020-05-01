a26c55b2348254112b5d14e1ff8c84a5c4576a7a
tylertjburns <52676131+tylertjburns@users.noreply.github.com>
2020-05-01 11:05:34 -0500

    add __init__

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='coopstructs',
-      version='0.1',
+      version='0.3',
       description='basic commonly used data structures for python applications',
       url='https://github.com/tylertjburns/datastructs',
       author='tburns',
